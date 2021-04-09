045aed1bce342200d1db6c4d37d168da74110d3c
Connor <s11098563@students.duvalschools.org>
2021-04-09 14:01:50 -0400

    [8B] v0.1
    Created calculator to find the speed by using input distance divided by input time values.

---
+++ speed.py
@@ -0,0 +1,16 @@
+# Speed Calculator, Connor Cook, April 09, 2021, 2:00PM v0.1
+
+keep_going = "Yes"
+
+while keep_going == "Yes" or keep_going == "yes":
+
+    distance = float(input("Please enter the distance and press enter.\n"))
+    time = float(input("Please enter the time and press enter.\n"))
+
+    speed = distance / time
+
+    print(f"Using the distance of {distance} meters and the time of {time} seconds, the speed is {speed} m/s.\n")
+
+    keep_going = input("Would you like to do another calculation? Type yes or no and push Enter.\n")
+
+print("Thank you for using this calculator.\n")
